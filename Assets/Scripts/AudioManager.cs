416c052c186aa3f1edf190f124f9b392c561499b
MatthewRobertMason <Git@matthewmason.ca>
2018-12-02 18:41:57 -0400

    Music Accusations

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{    
+    static private GameObject self;
+    private void Awake()
+    {
+        if (self == null)
+        {
+            self = gameObject;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public AudioClip purgatoryMusic;
+    public AudioClip cultMusic;
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start ()
+    {
+        audioSource = this.GetComponent<AudioSource>();
+
+        audioSource.clip = purgatoryMusic;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+	
+    public void SwitchMusicToPurgatoryMusic()
+    {
+        audioSource.Stop();
+        float time = 0.0f;
+        time = audioSource.time;
+        audioSource.clip = purgatoryMusic;
+        audioSource.time = time;
+        audioSource.Play();
+    }
+
+    public void SwitchMusicToCultMusic()
+    {
+        audioSource.Stop();
+        float time = 0.0f;
+        time = audioSource.time;
+        audioSource.clip = cultMusic;
+        audioSource.time = time;
+        audioSource.Play();
+    }
+}
---
+++ Assets/Scripts/OnLevelStart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnLevelStart : MonoBehaviour
+{
+    public bool isPurgatory = false;
+	
+	void Start ()
+    {
+        if (isPurgatory)
+        {
+            FindObjectOfType<AudioManager>().SwitchMusicToPurgatoryMusic();
+        }
+        else
+        {
+            FindObjectOfType<AudioManager>().SwitchMusicToCultMusic();
+        }
+	}
+}
