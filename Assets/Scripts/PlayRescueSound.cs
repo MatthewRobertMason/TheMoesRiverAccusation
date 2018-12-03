ec63d3ba9fda37a6e2a86ee725e1d03787bcce33
Adam Douglass <douglass@malloc.ca>
2018-12-03 18:50:00 -0500

    Fix pickup sound

---
+++ Assets/Scripts/PlayRescueSound.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayRescueSound : MonoBehaviour {
+    private AudioSource audioSource;
+    public AudioClip Rescue;
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        PlaySound(Rescue);
+        Invoke("Remove", 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Remove()
+    {
+        Destroy(gameObject);
+    }
+
+    private void PlaySound(AudioClip clip)
+    {
+        audioSource.Stop();
+        audioSource.clip = clip;
+        audioSource.loop = false;
+        audioSource.Play();
+    }
+}
