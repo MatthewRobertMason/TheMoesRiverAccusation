f1719a31272346cef01568e324db4ce70b99a1d7
MatthewRobertMason <Git@matthewmason.ca>
2018-12-11 20:52:54 -0400

    Lighting and prefab updates

---
+++ Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    public float variationInScale = 0.1f;
+
+    private float xScale = 0.0f;
+    private float yScale = 0.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        xScale = this.transform.localScale.x;
+        yScale = this.transform.localScale.y;   
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float changeAmount = (variationInScale / 10.0f);
+        float change = Random.Range(-changeAmount, changeAmount);
+
+        float x = this.transform.localScale.x + change;
+        float y = this.transform.localScale.y + change;
+        
+        x = Mathf.Clamp(x, xScale - variationInScale, xScale + variationInScale);
+        y = Mathf.Clamp(y, yScale - variationInScale, yScale + variationInScale);
+
+        this.transform.localScale = new Vector3(x, y, 1.0f);
+    }
+}
---
+++ Assets/Scripts/ShadowLight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ShadowLight : MonoBehaviour
+{
+    public Tilemap lightBlocking;
+
+	void Start ()
+    {
+		
+	}
+	
+
+	void Update ()
+    {
+		
+	}
+}
